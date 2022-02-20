9b252e3399c704769b4633644644291a9c8627ca
Shajib <sajidislam729@gmail.com>
2022-02-20 21:55:22 +0600

    manifest.json updated

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
 
 export default function Home() {
+
+  // const CLIENT='https://shajib-blog.herokuapp.com'
+
+  const getData = async () => {
+    const res = await fetch(`${process.env.CLIENT}/api/post/get_posts`, {
+      method: "get",
+    })
+    const data = await res.json()
+    console.log(data);
+  }
+
+  useEffect(() => {
+    getData()
+  },[])
+
   return (
     <div>
       <Head>
